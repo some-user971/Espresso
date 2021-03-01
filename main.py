ba14b6808f1d6f3fa8f11f7ea975a8526f87e2a4
Timur <timuriskin@yandex.ry>
2021-03-01 22:39:47 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("cofee.sqlite")
+        self.pushButton.clicked.connect(self.select_data)
+        # По умолчанию будем выводить все данные из таблицы films
+        self.textEdit.setPlainText("SELECT * FROM Cofees")
+        self.select_data()
+
+    def select_data(self):
+        # Получим результат запроса,
+        # который ввели в текстовое поле
+        query = self.textEdit.toPlainText()
+        res = self.connection.cursor().execute(query).fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
